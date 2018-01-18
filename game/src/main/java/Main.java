f4d008ff4eec7e784c1a2b2abe02101e4c96762a
NetasDev <tobiaskaemmerling@web.de>
2018-01-18 15:45:36 +0100

    inital comit

---
+++ game/src/main/java/Main.java
@@ -0,0 +1,20 @@
+import javax.swing.*;
+
+public class Main extends JPanel {
+
+
+    /* public void paint(Graphics g){
+
+
+
+    }*/
+
+
+    public static void main(String[] args){
+        JFrame frame = new JFrame("Unser game");
+        frame.setSize(1000,1000);
+        frame.setVisible(true);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+    }
+}
